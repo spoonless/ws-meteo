3e816fd1aa98e87f693e4363df6e13efcdbd61b4
David <dagaydevel@free.fr>
2019-07-09 21:51:47 +0200

    suppression des fichiers pour un template de projet

--- src/main/java/com/mymeteo/meteo/package-info.java
+++
@@ -1,4 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.mymeteo.com/meteo", elementFormDefault=XmlNsForm.QUALIFIED)
-package com.mymeteo.meteo;
-import javax.xml.bind.annotation.XmlNsForm;
-
